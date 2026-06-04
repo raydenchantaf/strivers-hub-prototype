603bef62f3ffd7dedc0768ed0332f9db13d9f179
raydenchan <squallray@hotmail.com>
2026-06-04 16:20:33 +0800

    update footer notes

--- context/translations/footer.ts
+++ context/translations/footer.ts
@@ -4,7 +4,7 @@ export const footer = {
     "footer.implementing_partner": "Implementing Partner",
     "footer.partnership_with":      "In partnership with",
     "footer.tagline":           "Strivers\' Hub empowers Malaysian women entrepreneurs by closing the digitalization gap. We provide tailored solutions, tools, and networks that help women-led small businesses thrive in the digital economy and unlock their full potential.",
-    "footer.rights":            "© 2024 - 2026 Mastercard Strive Malaysia",
+    "footer.rights":            "© 2024 - 2026 Striver's Hub Malaysia",
     "footer.privacyPolicy":     "Privacy Policy",
 
   },
@@ -13,7 +13,7 @@ export const footer = {
     "footer.implementing_partner": "Rakan Pelaksana",
     "footer.partnership_with":     "Dengan kerjasama",
     "footer.tagline":           "Strivers\' Hub memperkasakan usahawanita Malaysia dengan merapatkan jurang digitalisasi. Kami menyediakan penyelesaian, alat, dan rangkaian yang disesuaikan untuk membantu perniagaan kecil yang dipimpin wanita berkembang dalam ekonomi digital dan mencapai potensi penuh mereka.",
-    "footer.rights":            "© 2024 - 2026 Mastercard Strive Malaysia",
+    "footer.rights":            "© 2024 - 2026 Striver's Hub Malaysia",
     "footer.privacyPolicy":     "Dasar Privasi",
   },
 };
